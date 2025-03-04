bad8bda32cd132be72a3716726e226e4013579ce
Ajeet <ajeetsamar9546@gmail.com>
2025-03-04 21:53:09 +0530

    version 2 mended 1

--- src/Games/Aviator/PredictionHistory.jsx
+++ src/Games/Aviator/PredictionHistory.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const PredictionHistory = () => {
     const [inputValue, setInputValue] = useState("");
-    const [values, setValues] = useState(["2.5x", "33.2x", "4.01x","6.8x","7.5x","3.2x","2.3x"]);
+    const [values, setValues] = useState(["2.5x", "33.2x", "4.01x", "6.8x", "7.5x", "3.2x", "2.3x"]);
 
     const handleAddValue = () => {
         if (inputValue.trim() === "") return;
@@ -15,10 +15,10 @@ const PredictionHistory = () => {
 
     return (
         <div>
-            <div style={{display:"flex"}}>
+            <div style={{ display: "flex" }}>
                 {values.map((val, index) => (
-                    <div key={index} style={{ marginLeft:"3px",marginRight:"3px", color: "#4287f5",height:"10%",width:"13%",background:"black",borderRadius:"10px",display:"flex",alignItems:"center",justifyContent:"center" }}>
-                        {val}
+                    <div key={index} style={{ marginLeft: "3px", marginRight: "3px", color: "#4287f5", height: "10%", width: "13%", background: "black", borderRadius: "10px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <h2 style={{fontSize: "2vh" }}>{val}</h2>
                     </div>
                 ))}
             </div>
